edade20c4c3efd587af10a55828300a1e19d1415
Bruce <brucehuynh67@gmail.com>
2022-12-01 14:54:01 -0500

    butterfly tb

---
+++ sim/butterfly_tb.v
@@ -0,0 +1,13 @@
+module butterfly_tb();
+
+	reg signed [15:0] A, B, C, D;
+	reg signed [7:0]  W;
+	wire signed [15:0]  out1, out2, out3, out4;
+
+
+	butterfly b0(
+		.A(A), .B(B), .C(C), .D(D), .W(W), .out1(out1), .out2(out2), .out3(out3), .out4(out4)
+	);
+
+
+endmodule
