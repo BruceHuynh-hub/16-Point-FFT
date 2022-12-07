2331ce02fc4225bf632f578cbfc04ab85ef76cb3
Bruce <brucehuynh67@gmail.com>
2022-12-07 01:55:25 -0500

    butterfly c

--- software/butterfly/butterfly_fft.h
+++ software/butterfly/butterfly_fft.h
@@ -1,15 +1,15 @@
 #ifndef BUTTERFLY_FFT_H
 #define BUTTERFLY_FFT_H
 
-#define FFT_OUT1   (*(volatile signed *) 0x110)
-#define FFT_OUT2   (*(volatile signed *) 0x112)
-#define FFT_OUT3   (*(volatile signed *) 0x114)
-#define FFT_OUT4   (*(volatile signed *) 0x116)
-#define FFT_OUT5   (*(volatile signed *) 0x118)
-#define FFT_OUT6   (*(volatile signed *) 0x11A)
-#define FFT_OUT7   (*(volatile signed *) 0x11C)
-#define FFT_OUT8   (*(volatile signed *) 0x11E)
-#define FFT_OUT9   (*(volatile signed *) 0x120)
+#define FFT_OUT0   (*(volatile signed *) 0x110)
+#define FFT_OUT1   (*(volatile signed *) 0x112)
+#define FFT_OUT2   (*(volatile signed *) 0x114)
+#define FFT_OUT3   (*(volatile signed *) 0x116)
+#define FFT_OUT4   (*(volatile signed *) 0x118)
+#define FFT_OUT5   (*(volatile signed *) 0x11A)
+#define FFT_OUT6   (*(volatile signed *) 0x11C)
+#define FFT_OUT7   (*(volatile signed *) 0x11E)
+#define FFT_OUT8   (*(volatile signed *) 0x120)
 #define FFT_FIFO   (*(volatile signed *) 0x130)
 
 #endif
