9ebbfbb3afe1554fbc92e92a7d35f53aeec1d2fe
Bruce <brucehuynh67@gmail.com>
2022-12-07 00:36:29 -0500

    butterfly changes

--- software/butterfly/butterfly.v
+++ software/butterfly/butterfly.v
@@ -1,19 +1,29 @@
 module butterfly (
 	input signed [15:0] Ar, Br, Cr, Dr,
 	input signed [15:0] Ai, Bi, Ci, Di,
-	input signed [7:0] Wr, Wi, // twiddle factor
-	output signed[15:0] out1, out2, out3, out4
+	output signed[15:0] out1r, out2r, out3r, out4r,
+	output signed[15:0] out1i, out2i, out3i, out4i
 );
 
+	// define parameters for twiddle factor here
 
-	always @(A, B, C, D) begin
+	reg [15:0]
 
-		
-
+	always @(*) begin
 
 	end
 
+	assign out1r = Ar + Br + Cr + Dr;
+	assign out1i = Ai + Bi + Ci + Di;
 
+	assign out2r = Ar + Bi - Cr - Di;
+	assign out2i = Ai - Br - Ci + Dr;
+
+	assign out3r = Ar - Br + Cr - Dr;
+	assign out3i = Ai - Bi + Ci - Di;
+
+	assign out4r = Ar - Bi - Cr + Di;
+	assign out4i = Ai + Br - Ci - Dr;
 
 
 endmodule 
