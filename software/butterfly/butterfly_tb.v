900f0fa2984bf3e27df81fe59d74644aae6b078f
Bruce <brucehuynh67@gmail.com>
2022-12-07 20:37:55 -0500

    butterfly tb2

--- software/butterfly/butterfly_tb.v
+++ software/butterfly/butterfly_tb.v
@@ -9,8 +9,52 @@ module butterfly_tb();
 	butterfly but0 (.Ar(Ar), .Ai(Ai), .Br(Br), .Bi(Bi), .Cr(Cr), .Ci(Ci), Dr(Dr), Di(Di),
 	.out0r(out0r), .out0i(out0i), .out1r(out1r), .out1i(out1i), .out2r(out2r), .out2i(out2i), .out3r(out3r), .out3i(out3i));
 
+	initial begin
+        $dumpfile("cdtb.vcd");
+        $dumpvars(0,coeff_decomposer_tb);
 
+        Ar = ; 
+        Ai = ;
+        Br = ;
+        Bi = ;
+        Cr = ;
+        Ci = ;
+        Dr = ;
+        Di = ;
 
+        #20
 
+        Ar = ; 
+        Ai = ;
+        Br = ;
+        Bi = ;
+        Cr = ;
+        Ci = ;
+        Dr = ;
+        Di = ;
+
+        #20
+
+        Ar = ; 
+        Ai = ;
+        Br = ;
+        Bi = ;
+        Cr = ;
+        Ci = ;
+        Dr = ;
+        Di = ;
+
+        #20
+
+        Ar = ; 
+        Ai = ;
+        Br = ;
+        Bi = ;
+        Cr = ;
+        Ci = ;
+        Dr = ;
+        Di = ;
+        
+    end
 
 endmodule
