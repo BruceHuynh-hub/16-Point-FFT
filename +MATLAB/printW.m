7857adadebb0b426f6d43966738ecf461b985c54
Oleynick, Robert Parsons (100041528) <rpoleynick@wpi.edu>
2022-12-08 18:00:40 -0500

    Binary twiddle factors

--- +MATLAB/printW.m
+++ +MATLAB/printW.m
@@ -1,3 +1,5 @@
 for N = 1:10
-    fprintf("real(W) = %.12f, imag(W) = %.12f\n", real(W(N)), imag(W(N)));
+    re = dec2bin(real(W(N))*2^15, 16);
+    im = dec2bin(imag(W(N))*2^15, 16);
+    fprintf("N: %d || real(W) = %s, imag(W) = %s\n", N-1, re, im);
 end
