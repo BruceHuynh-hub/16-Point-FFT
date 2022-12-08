169a89b40920e475073203cc54e805f4d22309d2
Bruce <brucehuynh67@gmail.com>
2022-12-08 05:22:31 -0500

    signed multiplier tb

--- software/butterfly/signed_multiplier.v
+++ software/butterfly/signed_multiplier.v
@@ -7,27 +7,33 @@ module signed_multiplier(
 );
 
 
-	wire [15:0] din_stored;
-	wire [7:0] W_stored;
+	reg [15:0] din_stored;
+	reg [7:0] W_stored;
 	reg  [23:0] dinW;
 	reg  [23:0] dinW_2;
 	reg  [15:0] dinW_o;
+	reg [1:0] flag;
 
+	always @(din or W) begin
+	din_stored = (din[15]) ? ~din + 1'b1 : din; // if negative then flip
+	W_stored   = (W[7])    ? ~W + 1'b1 : W; // if negative then flip
 
-
-	assign din_stored = (din[15]) ? ~din + 1'b1 : din; // if negative then flip
-	assign W_stored = (W[15]) ? ~W + 1'b1 : W; // if negative then flip
-	always @(din) begin
-
-		if (din[15] + W[7]) begin
+		flag = (din[15] + W[7]);
+		if ((din[15] + W[7]) == 1'b1) begin
+			$display("negative detected");
 			dinW = din_stored * W_stored;
 			dinW_2 = ~dinW + 1'b1;
-			dinW_o = {dinW[23:15], dinW[14:7]};
+			dinW_o = dinW_2[21:6];
+
 		end
 		else begin
+			$display("negative not detected");
 			dinW = din_stored * W_stored;
-			dinW_o = {dinW[23:15], dinW[14:7]};
+			dinW_o = dinW[21:6];
+
 		end
 	end
+
+	assign dout = dinW_o;
 
 endmodule
---
+++ software/butterfly/signed_multiplier_tb.v
@@ -0,0 +1,47 @@
+module signed_multiplier_tb();
+
+
+	reg signed [15:0] din;
+	reg signed  [7:0] W;
+	wire signed [15:0] dout;
+
+	signed_multiplier sm(.din(din), .W(W), .dout(dout));
+
+	initial begin
+
+		din = 0;
+		W = 0;
+		$dumpfile("trace_sm.vcd");
+		$dumpvars(0, signed_multiplier_tb);
+		#20
+
+		din = 16'b00000100_00000000;
+		W =    8'b01_000000;
+		// 4 * 1 = 4
+		#20
+		din = 16'b00000100_00000000;
+		W =    8'b00_111111;
+		// 4 * 0.984375 = 3.9375
+		#20
+		din = 16'b00000100_00000000;
+		W =    8'b11_000000;
+		// 4 * -1 = -4
+		#20
+
+		din = 16'b11111100_00000000;
+		W =    8'b11_000000;
+		// -4 * -1 = 4
+		#20
+
+		din = 16'b10111100_00000000;
+		W =    8'b10_100000;
+		// -68 * -0.5
+		#20
+		$display(dout);
+		#50
+		$finish;
+	end
+
+
+
+endmodule 
