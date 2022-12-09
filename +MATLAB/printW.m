0047d1ac707371c5d2fe9731a4664f02b8a8d96a
Oleynick, Robert Parsons (100041528) <rpoleynick@wpi.edu>
2022-12-08 21:50:08 -0500

    Integer twiddle factors

--- +MATLAB/printW.m
+++ +MATLAB/printW.m
@@ -1,5 +1,8 @@
 for N = 1:10
-    re = dec2bin(real(W(N))*2^15, 16);
-    im = dec2bin(imag(W(N))*2^15, 16);
-    fprintf("N: %d || real(W) = %s, imag(W) = %s\n", N-1, re, im);
+    %re = dec2bin(real(W(N))*2^15, 16);
+    %im = dec2bin(imag(W(N))*2^15, 16);
+    re = round(real(W(N))*2^15);
+    im = round(imag(W(N))*2^15);
+    
+    fprintf("N: %d || real(W) = %6d, imag(W) = %6d\n", N-1, re, im);
 end
