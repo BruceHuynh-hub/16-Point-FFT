97a60166706267e67a1f72e0d3def0d4b3236885
Oleynick, Robert Parsons (100041528) <rpoleynick@wpi.edu>
2022-12-13 23:23:55 -0500

    MATLAB stuff

---
+++ +MATLAB/hex16fft.m
@@ -0,0 +1,13 @@
+A = (1:16).^2*10;
+F = fft(A);
+
+for n = 1:16
+    fprintf("r%X=%04X i%X=%04X\n", n-1, shortTwos(real(F(n))), n-1, shortTwos(imag(F(n))));
+end
+
+function n = shortTwos(n)
+    n = round(n);
+    if n < 0
+        n = 2^16 + n;
+    end
+end
--- +MATLAB/printW.m
+++ +MATLAB/printW.m
@@ -1,8 +1,10 @@
+W = exp(-1j*2*pi/16*(0:9));
 for N = 1:10
-    %re = dec2bin(real(W(N))*2^15, 16);
-    %im = dec2bin(imag(W(N))*2^15, 16);
-    re = round(real(W(N))*2^15);
-    im = round(imag(W(N))*2^15);
+    re = dec2bin(real(W(N))*2^15, 16);
+    im = dec2bin(imag(W(N))*2^15, 16);
+    %re = round(real(W(N))*2^15);
+    %im = round(imag(W(N))*2^15);
     
-    fprintf("N: %d || real(W) = %6d, imag(W) = %6d\n", N-1, re, im);
+    %fprintf("N: %d || real(W) = %6d, imag(W) = %6d\n", N-1, re, im);
+    fprintf("N: %d || real(W) = %s, imag(W) = %s\n", N-1, re, im);
 end
