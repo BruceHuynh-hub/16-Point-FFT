c2029cb5e5617c701ef1bc4e813a212e0e721947
Oleynick, Robert Parsons (100041528) <rpoleynick@wpi.edu>
2022-12-14 17:52:33 -0500

    Move test outputs to /Testing

--- Development/MATLAB/hex16fft.m
+++ Development/MATLAB/hex16fft.m
@@ -1,7 +1,7 @@
-A = ([13 14 15 16 0 0 0 0 0 0 0 0 0 0 0 0]).^2*10;
+A = (1:16).^2*10;
 F = fft(A);
 
-for n = 1:16
+for n = 1:9
     fprintf("r%X=%04X i%X=%04X\n", n-1, shortTwos(real(F(n))), n-1, shortTwos(imag(F(n))));
 end
 
